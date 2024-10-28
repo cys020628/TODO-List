3765042c6575fb4821f1a3fbf2e511024cf3474c
cys020628 <qweasd0628@>
2024-10-28 22:02:49 +0900

    프로젝트 초반 구조 만들어 업로드

---
+++ app/src/main/java/com/example/basictodoproject/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.basictodoproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.basictodoproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+
+    }
+
+    // 기본 앱 세팅 메서드
+    private fun setUpSetting() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
